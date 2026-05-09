f99fa8c4efd3c7ef1141d156258504158d688189
keerusandra <107774736+keerusandra@users.noreply.github.com>
2026-05-09 15:33:38 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 
+require('datejs');
 
+function combineUsers(...args) {
+  // Initialize return object
+  const combinedObject = {
+    users: []
+  };
+
+  // Loop through args and merge arrays
+  for (const arr of args) {
+    combinedObject.users = [
+      ...combinedObject.users,
+      ...arr
+    ];
+  }
+
+  // Add today's date
+  combinedObject.merge_date = Date.today().toString('M/d/yyyy');
+
+  // Return object
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
